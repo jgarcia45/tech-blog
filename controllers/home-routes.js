f5aa927293b416317ff4bd1c24b49517cc0edf71
Juan <jgarcia1545@gmail.com>
2022-05-13 17:47:11 -0700

    Created a signup handlebar, javascript file. Edited the login handlebar and home route. Added some csss

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -98,4 +98,14 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// GET Sign Up
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
 module.exports = router;
